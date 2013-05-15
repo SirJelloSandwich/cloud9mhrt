c096ccf5482ea76baacef2fbc4cdfe752d1d5d5d
Kevin W Palmer <kevinwpalmer@gmail.com>
2013-05-15 18:58:47 -0400

    Stuck on remember token and find by remember token.  Start at 8.2.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_save { |user| user.email = email.downcase }
-  #before_save :create_remember_token
+  before_save :create_remember_token
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
@@ -12,9 +12,8 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
   
- # private
-  
-#	def create_remember_token
-#		self.remember_token = SecureRandom.urlsafe_base64
-#	end
+ private  
+	def create_remember_token
+		self.remember_token = SecureRandom.urlsafe_base64
+	end
 end
